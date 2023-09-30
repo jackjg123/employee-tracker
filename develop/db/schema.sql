17cb210276f624be9ccec21855ebe67d63852a95
jackjg123 <131559096+jackjg123@users.noreply.github.com>
2023-09-30 16:23:00 -0400

    Added table of role to schema and connected   table  primary key uid=197609(johnj) gid=197609 groups=197609  to  table through foreign key

--- develop/db/schema.sql
+++ develop/db/schema.sql
@@ -8,3 +8,11 @@ CREATE TABLE department (
   departments_name VARCHAR(30),
 )
 
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30).
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+  ON DELETE SET NULL
+);
